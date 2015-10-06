ad0071c6ed77f9cc118c0c3ffbb5d53570c8f74c
augstm <augstm@PC-B124-03.iut-valence.fr>
2015-10-06 15:59:09 +0200

    method renamed

--- src/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRose.java
+++ src/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRose.java
@@ -24,10 +24,11 @@ public class GildedRose
 		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
 		items.add(new Item("Conjured Mana Cake", 3, 6));
 
-		updateQuality();
+		updateShop();
 	}
 
-	public static void updateQuality()
+	// update the shop (each item)
+	public static void updateShop()
 	{
 		for (int i = 0; i < items.size(); i++)
 		{
